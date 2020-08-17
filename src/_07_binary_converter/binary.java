3c66a870007bca6a39b1a727091c8978d0b0e5ff
aLeXparK037 <65091873+aLeXparK037@users.noreply.github.com>
2020-08-17 11:03:56 -0700

    binary conversion

--- src/_07_binary_converter/binary.java
+++ src/_07_binary_converter/binary.java
@@ -1,5 +1,90 @@
 package _07_binary_converter;
 
-public class binary {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class binary implements MouseListener {
+	JTextField answer = new JTextField(20);
+	
+	JLabel label = new JLabel();
+public void base() {
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	frame.setVisible(true);
+	frame.add(panel);
+	panel.add(answer);
+	JButton button = new JButton();
+	button.setText("button");
+	panel.add(button);
+	button.addMouseListener(this);
+	panel.add(label);
+	
+	
+	
+	
+	
+	frame.pack();
+}
+
+	String convert(String input) {
+	    if(input.length() != 8){
+	        JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+	        return "-";
+	    }
+	    String binary = "[0-1]+";//must contain numbers in the given range
+	    if (!input.matches(binary)) {
+	        JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+	        return "-";   
+	    }
+	    try {
+	        int asciiValue = Integer.parseInt(input, 2);
+	        char theLetter = (char) asciiValue;
+	        return "" + theLetter;
+	    } catch (Exception e) {
+	        JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+	        return "-";
+	    }
+	}
+
+
+
+@Override
+public void mouseClicked(MouseEvent e) {
+	// TODO Auto-generated method stub
+	String answers = answer.getText();
+	String ascii = convert(answers);
+	label.setText(ascii);
+}
+
+@Override
+public void mousePressed(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseReleased(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseEntered(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseExited(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
 }
---
+++ src/_07_binary_converter/binaryconverterrunner.java
@@ -0,0 +1,10 @@
+package _07_binary_converter;
+
+
+
+public class binaryconverterrunner {
+public static void main(String[] args) {
+	new binary().base();
+	
+}
+}
